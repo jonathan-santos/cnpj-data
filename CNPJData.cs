f79f7df005427ba6559f698295a4680286ff65b4
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2021-01-08 19:03:11 -0300

    Created CNPJData class and implemented it's use in Program

---
+++ CNPJData.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class CNPJData
+{
+    string _dataFolder = "Data";
+    int[] _cnpjDataFilesToDownload = new int[] { 1 };
+    string _uncompressedFilesFolder = "uncompressed";
+
+    public List<Company> Companies { get; set; }
+    public List<Member> Members { get; set; }
+
+    public CNPJData()
+    {
+        Companies = new List<Company>();
+        Members = new List<Member>();
+    }
+
+    public async Task GetCNPJData()
+    {
+        CreateDataFolders();
+
+        await RetrieveDataFiles();
+
+        foreach(var file in Directory.GetFiles($"{_dataFolder}/{_uncompressedFilesFolder}"))
+            ParseCPNJDataFromFile(file);
+    }
+
+    void CreateDataFolders()
+    {
+        if (!Directory.Exists(_dataFolder))
+            Directory.CreateDirectory(_dataFolder);
+
+        if (!Directory.Exists($"{_dataFolder}/{_uncompressedFilesFolder}"))
+            Directory.CreateDirectory($"{_dataFolder}/{_uncompressedFilesFolder}");
+    }
+
+    async Task RetrieveDataFiles()
+    {
+        foreach (var fileNumber in _cnpjDataFilesToDownload)
+        {
+            var file = $"DADOS_ABERTOS_CNPJ_{fileNumber.ToString("D2")}.zip";
+
+            if (!File.Exists($"{_dataFolder}/{file}"))
+            {
+                Console.WriteLine($"{file} File doesn't exist, downloading...");
+                using var client = new HttpClient();
+                client.Timeout = new TimeSpan(1, 0, 0);
+
+                var result = await client.GetAsync($"http://200.152.38.155/CNPJ/{file}");
+
+                if (result.StatusCode != HttpStatusCode.OK)
+                    throw new Exception("GET CNPJ Data API Request failed");
+
+                using (var fs = new FileStream($"{_dataFolder}/{file}", FileMode.CreateNew))
+                    await result.Content.CopyToAsync(fs);
+            }
+
+            string dataFile;
+            using (var archive = new ZipArchive(File.Open($"{_dataFolder}/{file}", FileMode.Open, FileAccess.Read), ZipArchiveMode.Read))
+                dataFile = archive.Entries.First().FullName;
+
+            if (!File.Exists($"{_dataFolder}/{_uncompressedFilesFolder}/{dataFile}"))
+            {
+                Console.WriteLine($"{file} not extracted yet, extracting...");
+                ZipFile.ExtractToDirectory($"{_dataFolder}/{file}", $"{_dataFolder}/{_uncompressedFilesFolder}");
+            }
+        }
+    }
+
+    void ParseCPNJDataFromFile(string file)
+    {
+        Console.WriteLine($"Reading {file} contents...");
+
+        using(var reader = new StreamReader(file))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+
+                if (line[0] == '1')
+                    Companies.Add(ParseCompanyFromLine(line));
+                else if (line[0] == '2')
+                    Members.Add(ParseMemberFromLine(line));
+            }
+        }
+
+        Console.WriteLine($"Finished reading {file} contents");
+    }
+
+    Company ParseCompanyFromLine(string line)
+    {
+        return new Company()
+        {
+            CNPJ = line.Substring(3, 14),
+            Type = (CompanyType) int.Parse(line.Substring(17, 1)),
+            BusinessName = line.Substring(18, 150).Trim(),
+            FantasyName = line.Substring(168, 55).Trim(),
+            SocialCapital = double.Parse(line.Substring(891, 14)),
+            RegistrationSituation = (Situation) int.Parse(line.Substring(223, 2)),
+            RegistrationSituationDate = DateTime.ParseExact(line.Substring(225, 8), "yyyyMMdd", null),
+            Cep = line.Substring(674, 8)
+        };
+    }
+
+    Member ParseMemberFromLine(string line)
+    {
+        return new Member()
+        {
+            Identifier = (MemberIdentifier) int.Parse(line.Substring(17, 1)),
+            Name = line.Substring(18, 150).Trim(),
+            CNPJCPF = line.Substring(168, 14)
+        };
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -12,84 +6,10 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        await GetCNPJData();
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    static async Task GetCNPJData()
-    {
-        if (!File.Exists("Data/DADOS_ABERTOS_CNPJ_01.zip"))
-        {
-            Console.WriteLine("Data/DADOS_ABERTOS_CNPJ_01.zip File doesn't exist, downloading...");
-            using var client = new HttpClient();
-            client.Timeout = new TimeSpan(1, 0, 0);
-
-            var result = await client.GetAsync("http://200.152.38.155/CNPJ/DADOS_ABERTOS_CNPJ_01.zip");
-
-            if (result.StatusCode != HttpStatusCode.OK)
-                throw new Exception("GET CNPJ Data API Request failed");
-
-            using (var fs = new FileStream("Data/DADOS_ABERTOS_CNPJ_01.zip", FileMode.CreateNew))
-                await result.Content.CopyToAsync(fs);
-        }
-
-        if (!Directory.Exists("Data/01"))
-        {
-            Console.WriteLine("Data/DADOS_ABERTOS_CNPJ_01.zip not extracted yet, extracting...");
-            ZipFile.ExtractToDirectory("Data/DADOS_ABERTOS_CNPJ_01.zip", "Data/01");
-        }
-        
-        foreach(var file in Directory.GetFiles("./Data/01"))
-            ReadCNPJData(file);
-    }
-
-    static void ReadCNPJData(string file)
-    {
-        Console.WriteLine($"Reading {file} contents...");
-
-        var companies = new List<Company>();
-        var members = new List<Member>();
-
-        using(var reader = new StreamReader(file))
-        {
-            while (!reader.EndOfStream)
-            {
-                var line = reader.ReadLine();
-                if (line[0] == '1')
-                {
-                    companies.Add(new Company()
-                    {
-                        CNPJ = line.Substring(3, 14),
-                        Type = (CompanyType) int.Parse(line.Substring(17, 1)),
-                        BusinessName = line.Substring(18, 150).Trim(),
-                        FantasyName = line.Substring(168, 55).Trim(),
-                        SocialCapital = double.Parse(line.Substring(891, 14)),
-                        RegistrationSituation = (Situation) int.Parse(line.Substring(223, 2)),
-                        RegistrationSituationDate = DateTime.ParseExact(line.Substring(225, 8), "yyyyMMdd", null),
-                        Cep = line.Substring(674, 8)
-                    });
-                }
-                else if (line[0] == '2')
-                {
-                    members.Add(new Member()
-                    {
-                        Identifier = (MemberIdentifier) int.Parse(line.Substring(17, 1)),
-                        Name = line.Substring(18, 150).Trim(),
-                        CNPJCPF = line.Substring(168, 14)
-                    });
-                }
-            }
-        }
-
-        Console.WriteLine($"Finished reading {file} contents");
-
-        // foreach (var company in companies)
-        //     Console.WriteLine($"{company.CNPJ} | {company.BusinessName} | {company.Type}");
-
-        // foreach (var member in members)
-        //     Console.WriteLine($"{member.CNPJCPF} | {member.Name} | {member.Identifier}");
-
-    }
+        var cnpjData = new CNPJData();
+        await cnpjData.GetCNPJData();
+        // CreateHostBuilder(args).Build().Run();
+    }    
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
